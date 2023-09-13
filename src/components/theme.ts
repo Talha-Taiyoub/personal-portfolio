f428225464c660f0cabe4b8f9fdf95db52729ed6
TalhaAnik56 <aniktalha@gmail.com>
2023-09-14 03:51:42 +0600

    helk

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -3,8 +3,6 @@ import { extendTheme} from "@chakra-ui/react";
 const styles={
     global:{
         body:{
-            // backgroundColor:"#2b273f"
-             //backgroundColor:"#454d66"
             backgroundColor:"##6b7db3"
         }
     }
