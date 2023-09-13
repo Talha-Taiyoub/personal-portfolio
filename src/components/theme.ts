eecbe149cee9e21949bf36dab71fe22b3d5315f9
TalhaAnik56 <aniktalha@gmail.com>
2023-09-14 04:23:24 +0600

    lastCheck

--- src/components/theme.ts
+++ src/components/theme.ts
@@ -1,15 +1,13 @@
-import { extendTheme} from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const styles={
-    global:{
-        body:{
-            backgroundColor:"##6b7db3"
-        }
-    }
-}
+const theme = extendTheme({
+  styles: {
+    global: () => ({
+      body: {
+        backgroundColor: "##6b7db3",
+      },
+    }),
+  },
+});
 
-const theme=extendTheme({styles})
-
-
-export default theme
-
+export default theme;
