d507dc852bff4c1fd72b97bd1c4346d88b2ffcf5
TalhaAnik56 <aniktalha@gmail.com>
2023-10-23 15:07:45 +0600

    refactored social media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -4,96 +4,55 @@ import { AiFillLinkedin } from "react-icons/ai";
 import { FaBitbucket } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 
+const socials = [
+  {
+    id: 1,
+    link: "https://www.facebook.com/talha.anik.98/",
+    icon: BsFacebook,
+  },
+  {
+    id: 2,
+    link: "https://github.com/TalhaAnik56",
+    icon: BsGithub,
+  },
+  {
+    id: 3,
+    link: "https://bitbucket.org/talhataiyoub/workspace/overview/",
+    icon: FaBitbucket,
+  },
+  {
+    id: 4,
+    link: "https://www.linkedin.com/in/talha-taiyoub-5716aa18b/",
+    icon: AiFillLinkedin,
+  },
+  {
+    id: 5,
+    link: "mailto:aniktalha@gmail.com?subject=Wanna%20Work%20Together",
+    icon: SiGmail,
+  },
+  { id: 6, link: "https://www.instagram.com/talha__anik/", icon: BsInstagram },
+];
+
 const SocialMedia = () => {
   return (
     <Box display={"flex"} justifyContent={"center"}>
-      <HStack spacing={{ base: 4, md: 8, lg: 10 }}>
-        <Link href="https://www.facebook.com/talha.anik.98/" target="_blank">
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize="30px" as={BsFacebook}></Icon>
-          </Circle>
-        </Link>
-
-        <Link href="https://github.com/TalhaAnik56" target="_blank">
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize={"30px"} as={BsGithub}></Icon>
-          </Circle>
-        </Link>
-
-        <Link
-          href="https://bitbucket.org/talhataiyoub/workspace/overview/"
-          target="_blank"
-        >
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize={"30px"} as={FaBitbucket}></Icon>
-          </Circle>
-        </Link>
-
-        <Link
-          href="https://www.linkedin.com/in/talha-taiyoub-5716aa18b/"
-          target="_blank"
-        >
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize={"30px"} as={AiFillLinkedin}></Icon>
-          </Circle>
-        </Link>
-
-        <Link
-          href="mailto:aniktalha@gmail.com?subject=Wanna%20Work%20Together"
-          target="_blank"
-        >
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize={"30px"} as={SiGmail}></Icon>
-          </Circle>
-        </Link>
-
-        <Link href="https://www.instagram.com/talha__anik/" target="_blank">
-          <Circle
-            size={{ base: "40px", md: "44px" }}
-            bg={"gray"}
-            _hover={{
-              backgroundColor: "white",
-              cursor: "pointer",
-            }}
-          >
-            <Icon color={"#141c3a"} boxSize={"30px"} as={BsInstagram}></Icon>
-          </Circle>
-        </Link>
+      <HStack spacing={{ base: 4, md: 8, lg: 10 }} paddingTop={4}>
+        {socials.map((social) => (
+          <Link key={social.id} href={social.link} target="blank">
+            <Circle
+              size={{ base: "40px", md: "44px" }}
+              bg={"gray"}
+              _hover={{
+                backgroundColor: "white",
+                cursor: "pointer",
+                transform: "scale(1.08)",
+                transition: "transform .15s ease-in",
+              }}
+            >
+              <Icon color={"#141c3a"} boxSize="30px" as={social.icon}></Icon>
+            </Circle>
+          </Link>
+        ))}
       </HStack>
     </Box>
   );
