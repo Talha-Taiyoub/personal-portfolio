ce6a264e77c340657ffea25bcc445fc5d43f2477
TalhaAnik56 <aniktalha@gmail.com>
2023-11-10 05:00:46 +0600

    fixed the error of Errorpage

---
+++ src/components/SimpleNavbar.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import { HStack, Image } from "@chakra-ui/react";
+import mainLogo from "../assets/akatsuki.svg";
+
+const SimpleNavbar = () => {
+  return (
+    <HStack justifyContent={"space-between"} paddingTop={{ base: 2, md: 3 }}>
+      <Link to={"/"}>
+        <Image
+          src={mainLogo}
+          boxSize={{ base: "60px", lg: "80px" }}
+          objectFit={"cover"}
+          borderRadius={"md"}
+        />
+      </Link>
+    </HStack>
+  );
+};
+
+export default SimpleNavbar;
--- src/pages/Errorpage.tsx
+++ src/pages/Errorpage.tsx
@@ -1,17 +1,13 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import Navbar from "../components/Navbar";
-
+import SimpleNavbar from "../components/SimpleNavbar";
 
 const ErrorPage = () => {
   const error = useRouteError();
   return (
-    <Box paddingX={{base:6,lg:20,}}>
-      <Navbar />
-      <Box
-        display={"flex"}
-        justifyContent={"center"}
-      >
+    <Box paddingX={{ base: 6, lg: 20 }}>
+      <SimpleNavbar />
+      <Box display={"flex"} justifyContent={"center"}>
         <Box>
           <Heading color={"red.700"} fontSize={"7xl"} as={"h1"}>
             Oops!
